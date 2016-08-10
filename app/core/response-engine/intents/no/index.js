e3114994c3f6f3b74dba8d2f1fbeb8f636ef3d75
Michael Beemer <beeme1mr@gmail.com>
2016-08-10 12:53:52 -0400

    DAV-71 End session if the user says no to the question "Is there anything else I can help with?"

--- app/core/response-engine/intents/no/index.js
+++ app/core/response-engine/intents/no/index.js
@@ -1,12 +1,51 @@
 'use strict';
 
-const common = require('../../utils/common'),
+const _ = require('lodash'),
+    BbPromise = require('bluebird'),
+    intents = require('../'),
+    common = require('../../utils/common'),
     logger = require('../../../../utils/logger');
 
 const process = function process(davis) {
     logger.info('Responding to a no intent.');
-    common.addTextResponse(davis.exchange, 'OK, no problem.  What else can I help you with?');
-    davis.exchange.request.finished = false;
+
+    const responses = [
+        'OK, no problem.  What else can I help you with?',
+        'Sure thing!  Is there anything else I can help you with?',
+        'OK, is there anything else?'
+    ];
+
+    const state = {
+        type: 'no',
+        next: {
+            no: 'stop'
+        }
+    };
+
+    return new BbPromise((resolve, reject) => {
+        davis.conversation.getHistory(2)
+            .then(result => {
+                const previousIntent = _.get(result, '[1].state.type');
+
+                if (previousIntent === 'no') {
+                    logger.debug('We were just in the no intent');
+                    const nextIntent = _.get(result, '[1].state.next.no');
+                    davis.exchange.intent.push(nextIntent);
+                    return intents.runIntent(nextIntent, davis);
+                } else {
+                    davis.exchange.state = state;
+                    davis.exchange.request.finished = false;
+                    return common.addTextResponse(davis.exchange, _.sample(responses));
+                }
+            })
+            .then(davis => {
+                resolve(davis);
+            })
+            .catch(err => {
+                logger.error(`Error loading the conversation history: ${err.message}`);
+                return reject(err);
+            });
+    });
 };
 
 module.exports.process = process;
