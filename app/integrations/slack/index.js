354f1a7ff6f0dde797bfa1f4c4e37fb8b4fec3c1
Cory Woolf <cory.woolf@dynatrace.com>
2016-08-15 20:30:08 +0000

    Slack integration can now handle multiple users correctly, user specific global variables moved into the new class SlackConversation. Inactivity timeout sends alert to user and gets reset when user is typing.

--- app/integrations/slack/index.js
+++ app/integrations/slack/index.js
@@ -14,16 +14,12 @@ module.exports = function (config) {
         interactive_replies: true
     });
     
-    let initialInteraction;
-    let initialResponse;
-    let lastInteractionTime;
-    let shouldEndSession;
-    let user;
     let bot;
     let botId;
+    const inactivityTimeoutTime = 60; // Seconds of inactivity until Davis goes to sleep
     
     // Launch phrases
-    let phrases = [
+    const phrases = [
         "hey davis",
         "okay davis",
         "ok davis",
@@ -126,169 +122,248 @@ module.exports = function (config) {
     });
     
     /**
-     * initConvo() is a recursive function that acts as a loop for continuing the conversation flow
-     * 
-     * @param response {Object}
-     * @param convo {Object}
-     */
-    let initConvo = function (err, convo) {
-        
-        showTypingNotification();
-        
-        lastInteractionTime = moment();
-        
-        phrases.forEach( (phrase) => {
-                    
-            if (initialInteraction.text.toLowerCase().includes(phrase)) {
-                
-                if (phrase.length == initialInteraction.text.trim().length) {
-                    initialInteraction.text = 'Start davis';
-                } else {
-                    initialInteraction.text = initialInteraction.text.toLowerCase().replace(phrase, ''); // Remove phrase
-                    initialInteraction.text = initialInteraction.text.replace(/(^\s*,)|(,\s*$)/g, ''); // Remove leading/trailing white-space and commas
-                }
-                
-            }
-            
-        });
-            
-        SlackService(config).askDavis(initialInteraction, user)
-        .then(resp => {
-            
-            logger.info('Sending a response back to the Slack service');
-            initialResponse = resp.response.outputSpeech.text;
-            shouldEndSession = resp.response.shouldEndSession;
-
-            convo.ask(initialResponse, (response, convo) => {
-                addToConvo(response, convo);
-            });
-            convo.next();
-            
-        })
-        .catch(err => {
-            logger.error('Unable to respond to the request received from Slack');
-            logger.error(err);
-        });
-        
-    };
-    
-    /**
-     * Recursive function that responds to a request and tells Slack when the conversation should continue (convo.ask) or end (convo.say)
+     * Makes Slack display that Davis is typing a message (similar to processing in web UI)
      */ 
-    let addToConvo = function (response, convo) {
-
-        // Timeout of 60 seconds
-        let isTimedOut = moment().subtract(60, 'seconds').isAfter(lastInteractionTime);
-        
-        // Reset last interaction timestamp
-        lastInteractionTime = moment();
-        
-        // if lastInteractionTime is more than 30 seconds ago, end conversation
-        if (shouldEndSession || isTimedOut) {
-            
-            logger.info('Slack: Conversation stopped');
-            convo.stop();
-            bot.destroy.bind(bot);
-            
-        } else {
-            
-            showTypingNotification();
-            
-            SlackService(config).askDavis(response, user)
-            .then(resp => {
-                
-                logger.info('Slack: Sending a response');
-                
-                shouldEndSession = resp.response.shouldEndSession;
-                
-                // if no followup question
-                if (shouldEndSession) {
-                    
-                    convo.say(resp.response.outputSpeech.text);
-                    convo.next();
-                    
-                } else {
-
-                    // Send response and listen for request
-                    convo.ask(resp.response.outputSpeech.text, (response, convo) => {
-                    
-                        addToConvo(response, convo);
-                        
-                    });
-                    convo.next();
-                    
-                }
-    
-            })
-            .catch(err => {
-                logger.error('Unable to respond to the request received from Slack');
-                logger.error(err);
-            });
-            
-        }
-        
-    };
-    
-    let startConvo = function (bot, message) {
-        
-        // Get Slack user details (for timezone)
-        if (!user) {
-            
-            getUserDetails(message.user)
-            .then( (details) => { 
-                
-                user = details;
-                bot.startConversation(message, initConvo);
-                
-            }).catch(err => {
-                logger.error('Error in getUserDetails');
-                logger.error(err);
-            });
-    
-        } else {
-            
-            bot.startConversation(message, initConvo);
-            
-        }
-        
-    }
-    
-    let showTypingNotification = function () {
+    let showTypingNotification = function (channel) {
         
         bot.say({
             type: "typing",
-            channel: initialInteraction.channel // a valid slack channel, group, mpim, or im ID
+            channel: channel // a valid slack channel, group, mpim, or im ID
         });
         
     }
     
-    controller.hears(['(.*)'], 'direct_message,direct_mention', (bot, message) => {
+    controller.hears(['(.*)'], 'direct_message', (bot, message) => {
         
-        logger.info('Slack: Starting public conversation (direct)');
-        initialInteraction = message;
-        startConvo(bot, message);
+        logger.info('Slack: Starting public conversation (direct_message)');
+        bot.startConversation(message, (err, convo) => {
+            let slackConvo = new SlackConversation(message, true);
+            slackConvo.initConvo(err, convo);
+        });
+        
+    });
+    
+    controller.hears(['(.*)'], 'direct_mention', (bot, message) => {
+        
+        logger.info('Slack: Starting public conversation (direct_mention)');
+        bot.startConversation(message, (err, convo) => {
+            let slackConvo = new SlackConversation(message, false);
+            slackConvo.initConvo(err, convo);
+        });
         
     });
     
     controller.hears(phrases, 'mention,ambient', (bot, message) => {
-        
-        logger.info('Slack: Starting public conversation (ambient)');
-        initialInteraction = message;
-        startConvo(bot, message);
-        
-    });
-    
-    controller.on('user_channel_join', (bot,message) => {
-        
-        logger.info('Slack: User joined channel, updating user details')
-        getUserDetails(message.user)
-        .then( (member) => {
-            user = member;  
-        }).catch(err => {
-            logger.error('Error in getUserDetails');
-            logger.error(err);
+
+        logger.info('Slack: Starting public conversation (mention,ambient)');
+        bot.startConversation(message, (err, convo) => {
+            let slackConvo = new SlackConversation(message, false);
+            slackConvo.initConvo(err, convo);
         });
         
     });
+    
+    /**
+     * Slack conversation
+     * Interacts with botkit to allow conversation flow within Slack
+     */
+    class SlackConversation {
+        
+        constructor(message, isDirectMessage) {
+            this.initialInteraction = message;
+            this.isDirectMessage = isDirectMessage;
+            this.initialResponse;
+            this.lastInteractionTime;
+            this.inactivityTimeout;
+            this.resetTimeout = false;
+            this.shouldEndSession;
+            this.user;
+            this.directPrefix = '';
+            this.convoEnded = false;
+        }
+            
+        /**
+         * Initializes Slack conversation
+         * 
+         * @param err {Object} - error(s)
+         * @param convo {Object} - conversation created by botkit
+         */
+        initConvo(err, convo) {
+            
+            showTypingNotification(this.initialInteraction.channel);
+            
+            this.lastInteractionTime = moment();
+
+            this.inactivityTimeout = this.setInactivityTimeout(convo);
+            
+            getUserDetails(this.initialInteraction.user).then( (details) => {
+                
+                this.user = details;
+                
+                // if not a direct message, use @username as message prefix
+                if (!this.isDirectMessage) {
+                    this.directPrefix = '@' + this.user.name + ' ';
+                }
+            
+                // Strip launch phrase or set to a launch intent compatible phrase
+                phrases.forEach( (phrase) => {
+                            
+                    if (this.initialInteraction.text.toLowerCase().includes(phrase)) {
+                        
+                        if (phrase.length == this.initialInteraction.text.trim().length) {
+                            
+                            // Only a launch phrase detected, use launch intent compatible phrase
+                            this.initialInteraction.text = 'Start davis';
+                            
+                        } else {
+                            
+                            // Strip launch phrase
+                            this.initialInteraction.text = this.initialInteraction.text.toLowerCase().replace(phrase, ''); // Remove phrase
+                            this.initialInteraction.text = this.initialInteraction.text.replace(/(^\s*,)|(,\s*$)/g, ''); // Remove leading/trailing white-space and commas
+                            
+                        }
+                        
+                    }
+                    
+                });
+                    
+                    
+                SlackService(config).askDavis(this.initialInteraction, this.user)
+                .then(resp => {
+                    
+                    logger.info('Sending a response back to the Slack service');
+                    this.initialResponse = resp.response.outputSpeech.text;
+                    this.shouldEndSession = resp.response.shouldEndSession;
+                    
+                    // Listen for typing event
+                    controller.on('user_typing', (bot,message) => {
+                
+                        if (message.user === this.user.id && this.inactivityTimeout && !this.convoEnded) {
+                            logger.info('Slack: User typing, resetting timeout');
+                            this.lastInteractionTime = moment();
+                            this.resetTimeout = true;
+                        }  
+                        
+                    });
+        
+                    convo.ask(this.directPrefix + this.initialResponse, (response, convo) => {
+                        clearTimeout(this.inactivityTimeout);
+                        this.addToConvo(response, convo);
+                    });
+                    convo.next();
+                    
+                })
+                .catch(err => {
+                    logger.error('Unable to respond to the request received from Slack');
+                    logger.error(err);
+                });
+            
+            });
+            
+        }
+        
+        /**
+         * Recursive method that responds to a request and tells Slack when 
+         * the conversation should continue (convo.ask) or end (convo.say, convo.stop)
+         * 
+         * @param {String} response - text to be sent to user in response to request
+         * @param {Object) convo - conversation object created by botkit
+         */ 
+        addToConvo(response, convo) {
+    
+            // Timeout of 60 seconds
+            let isTimedOut = moment().subtract(inactivityTimeoutTime, 'seconds').isAfter(this.lastInteractionTime);
+            
+            // Reset last interaction timestamp
+            this.lastInteractionTime = moment();
+            
+            this.inactivityTimeout = this.setInactivityTimeout(convo);
+            
+            // if lastInteractionTime is more than 30 seconds ago, end conversation
+            if (!this.isDirectMessage && (this.shouldEndSession || isTimedOut)) {
+                
+                logger.info('Slack: Conversation stopped');
+                convo.stop();
+                this.convoEnded = true;
+                clearTimeout(this.inactivityTimeout);
+                
+            } else {
+                
+                showTypingNotification(this.initialInteraction.channel);
+                
+                SlackService(config).askDavis(response, this.user)
+                .then(resp => {
+                    
+                    logger.info('Slack: Sending a response');
+                    
+                    this.shouldEndSession = resp.response.shouldEndSession;
+                    
+                    // if no followup question
+                    if (this.shouldEndSession) {
+                        
+                        convo.say(this.directPrefix + resp.response.outputSpeech.text);
+                        convo.next();
+                        clearTimeout(this.inactivityTimeout);
+                        
+                    } else {
+    
+                        // Send response and listen for request
+                        convo.ask(this.directPrefix + resp.response.outputSpeech.text, (response, convo) => {
+                        
+                            this.addToConvo(response, convo);
+                            
+                        });
+                        convo.next();
+                        clearTimeout(this.inactivityTimeout);
+                        
+                    }
+        
+                })
+                .catch(err => {
+                    logger.error('Unable to respond to the request received from Slack');
+                    logger.error(err);
+                });
+                
+            }
+            
+        }
+        
+        /**
+         * Recursive method that alerts the user when they've been inactive 
+         * for 60 seconds if they're not in direct message mode
+         * 
+         * @param {Object} convo - conversation object created by botkit
+         */
+        setInactivityTimeout(convo) {
+            
+            // Inactivity timeout message sender
+            return setTimeout( () => {
+                
+                // if not a direct message, let the user know they need to wake Davis
+                if(!this.isDirectMessage && !this.resetTimeout) {
+                    
+                    convo.say(this.directPrefix + ":ZZZ: I've fallen asleep");
+                    convo.next();
+                    this.convoEnded = true;
+                    clearTimeout(this.inactivityTimeout);
+                    
+                    // Allows convo.say to execute beforehand
+                    setTimeout( () => {
+                        convo.stop()
+                    }, 1000);
+                    
+                } else {
+                    
+                    this.resetTimeout = false;
+                    clearTimeout(this.inactivityTimeout);
+                    this.inactivityTimeout = this.setInactivityTimeout(convo);
+                    
+                }
+                
+            }, inactivityTimeoutTime * 1000);
+            
+        }
+    
+    }
     
 };
