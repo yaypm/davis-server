a4256b91ce663ebc0f968896f6eb288d79ec6f34
Michael Beemer <beeme1mr@gmail.com>
2016-08-18 17:08:46 -0400

    DAV-102 Datetime issue for "what happened last week"

--- demo/aliases/infrastructure.js
+++ demo/aliases/infrastructure.js
@@ -14,6 +14,13 @@ const infrastructure = [{
         visual: 'CouchDB'
     },
     aliases: []
+}, {
+    name: '192.168.118.68',
+    display: {
+        audible: 'VM ware Hypervisor',
+        visual: 'PowerEdge R420 VMware ESXi'
+    },
+    aliases: []
 }];
 
 module.exports = infrastructure;
