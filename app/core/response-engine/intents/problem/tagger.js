212e0a7f409d825839a9739c64e492faa398ad34
Michael Beemer <beeme1mr@gmail.com>
2016-09-15 16:33:47 -0400

    Move the getTense call to the shared section.

--- app/core/response-engine/intents/problem/tagger.js
+++ app/core/response-engine/intents/problem/tagger.js
@@ -9,28 +9,9 @@ const tagger = {
     tag: davis => {
         return {
             lang: common.getLanguage(davis.user),
-            tense: getTense(davis.exchange),
+            tense: common.getTense(davis.exchange),
             problems: getCount(davis.intentData)
         };
-    }
-};
-
-const getTense = function(exchange) {
-    let timeRange = _.get(exchange, 'request.analysed.timeRange', null);
-    if(isPresentTense(timeRange)) {
-        logger.debug('Problem is present tense');
-        return 'present';
-    } else if (isPastTense(timeRange.startTime, exchange.startTime)) {
-        logger.debug('Problem is past tense');
-        return 'past';
-    } else if (isFutureTense(timeRange.startTime, exchange.startTime)) {
-        logger.warn('The user is asking about a future problem.  Either they think we\'re psychic or we misunderstood the question.');
-        logger.warn(`They said '${exchange.request.text}' and we thought the requested start time was ${timeRange.startTime}`);
-        logger.warn(`which is after the current time ${exchange.startTime}`);
-        return 'future';
-    } else {
-        logger.debug('Unable to figure out a tense');
-        return null;
     }
 };
 
@@ -55,18 +36,6 @@ const getCount = function(intentData) {
         logger.warn('No problem data supplied!');
         return null;
     }
-};
-
-const isPresentTense = function(timeRange) {
-    return _.isNil(timeRange);
-};
-
-const isPastTense = function(startTime, endTime) {
-    return time.isBefore(startTime, endTime);
-};
-
-const isFutureTense = function(startTime, endTime) {
-    return !time.isBefore(startTime, endTime);
 };
 
 module.exports = tagger;
