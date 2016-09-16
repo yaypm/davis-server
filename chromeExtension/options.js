2c3775111a4e9a14d7a43f36a046f9c16b1550c3
Cory Woolf <cory.woolf@dynatrace.com>
2016-09-16 14:54:26 +0000

    Added debug mode to Chrome Extension

--- chromeExtension/options.js
+++ chromeExtension/options.js
@@ -1,10 +1,12 @@
 // Saves options to chrome.storage.sync.
 function save_options() {
   var davisUrl = document.getElementById('davisUrl').value.trim();
-  var userId = document.getElementById('userId').value.trim(); 
+  var userId = document.getElementById('userId').value.trim();
+  var debugMode = document.getElementById('debugMode').checked;
   chrome.storage.sync.set({
     davisUrl: davisUrl,
-    userId: userId
+    userId: userId,
+	debugMode: debugMode
   }, function() {
     // Update status to let user know options were saved.
     document.getElementById('status').style.display = 'block';
@@ -12,7 +14,8 @@ function save_options() {
     setTimeout(function() {
 		document.getElementById('status').style.display = 'none';
 		document.getElementById('save').style.display = 'block';
-    }, 750);
+		window.close();
+    }, 500);
   });
 }
 
@@ -20,24 +23,29 @@ function save_options() {
 function restore_options() {
   chrome.storage.sync.get({
 		davisUrl: '', 
-		userId: ''
+		userId: '',
+		debugMode: ''
 	}, function (options) {
 		document.getElementById('davisUrl').value = options.davisUrl; 
-		document.getElementById('userId').value = options.userId;		
+		document.getElementById('userId').value = options.userId;
+		document.getElementById('debugMode').checked = options.debugMode;
   });
 }
-
-// Event listeners for options page
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click', save_options);
-window.addEventListener("keypress", verifyFields, false);
-
 
 function verifyFields(e){
 	if (document.getElementById('davisUrl').value.length > 0 
 		&& document.getElementById('userId').value.length > 0) {
 		document.getElementById('save').disabled = false;
+		if (e.keyCode == 13) {
+			save_options();
+		}
 	} else {
 		document.getElementById('save').disabled = true;
 	}
 };
+
+// Event listeners for options page
+document.addEventListener('DOMContentLoaded', restore_options, false);
+document.getElementById('save').addEventListener('click', save_options, false);
+document.getElementById('debugMode').addEventListener('click', verifyFields, false);
+window.addEventListener("keypress", verifyFields, false);
