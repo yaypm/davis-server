b1e4df22815a7ae651ca721a7d2875ad171fbb80
Cory Woolf <cory.woolf@dynatrace.com>
2016-10-17 20:23:32 +0000

    Improved missingIntent message

--- app/core/response-engine/intents/missingIntent/index.js
+++ app/core/response-engine/intents/missingIntent/index.js
@@ -5,7 +5,7 @@ const _ = require('lodash'),
     logger = require('../../../../utils/logger');
 
 const process = function process(davis, intentName) {
-    const message = `Oh no! The "${intentName}" intent is defined in Wit (Natural Language Processing Service), but not on the Davis server. To avoid this error in the future, make sure you're running the latest version of Davis by running "npm update" within davis-server and restarting the instance.`;
+    const message = `Sorry, but I was unable to find the intent entitled "${intentName}". The intent is defined in Wit, but not on the Davis server. Make sure you're running the latest version of Davis by navigating to the davis-server directory. Next, run "npm update". Finally, restart the Davis server.`;
     common.addTextResponse(davis.exchange, message);
     davis.exchange.response.finished = true;
 };
