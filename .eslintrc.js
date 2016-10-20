0a75366ca325e4cdec7b3585c2ba946e815fae03
Michael Beemer <beeme1mr@gmail.com>
2016-10-19 22:31:20 -0400

    Updated user REST interface

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
   "extends": "airbnb",
   "plugins": [],
   "rules": {
+    "no-param-reassign": [2, { "props": false }],
+    "new-cap": "off",
     "func-names": "off",
     "strict": "off",
     "prefer-rest-params": "off",
