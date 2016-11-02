ad3c99086a3a005e05ead7276793d43bb3736f0c
Michael Beemer <beeme1mr@gmail.com>
2016-11-02 15:48:51 -0400

    Removed the constructor describe from the decide test

--- tests/classes/Decide.js
+++ tests/classes/Decide.js
@@ -7,21 +7,19 @@ const Decide = require('../../lib/classes/Decide')
 chai.should();
 
 describe('Decide', () => {
-  describe('#constructor()', () => {
-    const davis = new Davis();
-    const decide = new Decide(davis);
+  const davis = new Davis();
+  const decide = new Decide(davis);
 
-    it('should return one past problem', () => {
-      const output = decide.predict([
-        { tense: 'past',    problem: 'zero', output: { template: 'past/zero' }},
-        { tense: 'past',    problem: 'one',  output: { template: 'past/one' }},
-        { tense: 'past',    problem: 'two',  output: { template: 'past/two' }},
-        { tense: 'present', problem: 'zero', output: { template: 'present/zero' }},
-        { tense: 'present', problem: 'one',  output: { template: 'present/one' }},
-        { tense: 'present', problem: 'two',  output: { template: 'present/two' }},
-      ], { tense: 'past', problem: 'zero' });
+  it('should return one past problem', () => {
+    const output = decide.predict([
+      { tense: 'past',    problem: 'zero', output: { template: 'past/zero' }},
+      { tense: 'past',    problem: 'one',  output: { template: 'past/one' }},
+      { tense: 'past',    problem: 'two',  output: { template: 'past/two' }},
+      { tense: 'present', problem: 'zero', output: { template: 'present/zero' }},
+      { tense: 'present', problem: 'one',  output: { template: 'present/one' }},
+      { tense: 'present', problem: 'two',  output: { template: 'present/two' }},
+    ], { tense: 'past', problem: 'zero' });
 
-      output.template.should.equal('past/zero');
-    });
+    output.template.should.equal('past/zero');
   });
 });
