2d695517adc0009474f2da60dd02263f180980f4
Daniel Dyla <dyladan@gmail.com>
2016-11-07 09:18:54 -0500

    fix max-len linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,14 @@ module.exports = {
     "strict": "off",
     "prefer-rest-params": "off",
     "react/require-extension" : "off",
-    "import/no-extraneous-dependencies" : "off"
+    "import/no-extraneous-dependencies" : "off",
+    "max-len": ["error", {
+      "ignoreComments": true,
+      "ignoreTrailingComments": true,
+      "ignoreUrls": true,
+      "ignoreStrings": true,
+      "ignoreTemplateLiterals": true
+    }]
   },
   "env": {
        "mocha": true
