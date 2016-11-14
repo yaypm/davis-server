85901afe185f0b33461eed2b7da6e2829706b23d
Michael Beemer <beeme1mr@gmail.com>
2016-11-14 14:01:06 -0500

    added an npm update task

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,19 @@
 const gulp = require('gulp');
 const mocha = require('gulp-mocha');
 
-gulp.task('default', () => {
+gulp.task('npmUpdate', () => {
+  const update = require('gulp-update')();
+
+  gulp.watch('./package.json').on('change', (file) => {
+    update.write(file);
+  });
+});
+
+gulp.task('test', () => {
   return gulp.src(['tests/all.js'], { read: false })
     .pipe(mocha({
       reporter: 'spec',
     }));
 });
+
+gulp.task('default', ['npmUpdate', 'test']);
