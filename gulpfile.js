e9d78d6411541afc5072184b1b31b29b5042c1b4
Daniel Dyla <dyladan@gmail.com>
2016-12-08 16:38:30 -0500

    gulp compilation

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@
 const gulp = require('gulp');
 const mocha = require('gulp-mocha');
 const tsc = require('gulp-typescript');
+const uglify = require('gulp-uglify');
+const sourcemaps = require('gulp-sourcemaps');
+const rename = require('gulp-rename');
 
 gulp.task('update', () => {
   const update = require('gulp-update')();
@@ -14,16 +17,33 @@ gulp.task('update', () => {
 
 gulp.task('compile', () => {
   const tsProject = tsc.createProject('tsconfig.json');
+  const destinationFolder = 'web/dist';
   gulp.src([
     'web/src/**/*.ts',
     '!web/src/main.prod.ts',
-  ])
-    .pipe(tsc(tsProject))
-    .pipe(gulp.dest('web/dist'));
+    ])
+    .pipe(sourcemaps.init())
+      .pipe(tsProject())
+      .pipe(gulp.dest(destinationFolder))
+      .pipe(uglify({ preserveComments: 'license' }))
+    .pipe(sourcemaps.write('/maps'))
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(gulp.dest(destinationFolder));
+
+  gulp.src([
+    'web/src/systemjs.config.js',
+    'web/src/**/*.html',
+    ])
+    .pipe(gulp.dest(destinationFolder));
+
+  gulp.src([
+    'web/src/assets/**/*',
+    ])
+    .pipe(gulp.dest(destinationFolder + '/assets'));
 });
 
 
-gulp.task('test', ['compile'], () =>
+gulp.task('test', ['update'], () =>
    gulp.src(['tests/all.js'], { read: false })
     .pipe(mocha({
       reporter: 'spec',
@@ -35,5 +55,10 @@ gulp.task('test', ['compile'], () =>
       process.exit(0);
     })
 );
+
+gulp.task('watch', ['compile'], function() {
+    gulp.watch('web/src/**/*.ts', ['compile']);
+    gulp.watch('web/src/**/*.html', ['compile']);
+});
 
 gulp.task('default', ['compile', 'test']);
