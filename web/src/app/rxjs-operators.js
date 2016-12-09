f86dd19074308713a601d0f8a586f42ccc43b4bb
Daniel Dyla <dyladan@gmail.com>
2016-12-09 18:32:21 -0500

    delete bad js and map files, add asset --skip-ci

--- web/src/app/rxjs-operators.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// Statics
-require("rxjs/add/observable/throw");
-// Operators
-require("rxjs/add/operator/map");
-require("rxjs/add/operator/toPromise");
-//# sourceMappingURL=rxjs-operators.js.map
