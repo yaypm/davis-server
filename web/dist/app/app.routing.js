eb5e03a57217e2cea8e0c3f05961d1472263d8d9
Daniel Dyla <dyladan@gmail.com>
2016-12-13 13:34:59 -0500

    do not minify in dev

--- web/dist/app/app.routing.js
+++
@@ -1,2 +0,0 @@
-"use strict";var router_1=require("@angular/router"),AppRoutes=[];exports.AppRouting=router_1.RouterModule.forRoot(AppRoutes);
-//# sourceMappingURL=../maps/app/app.routing.js.map
--- web/dist/app/rxjs-operators.js
+++
@@ -1,2 +0,0 @@
-"use strict";require("rxjs/add/observable/throw"),require("rxjs/add/operator/map"),require("rxjs/add/operator/toPromise");
-//# sourceMappingURL=../maps/app/rxjs-operators.js.map
