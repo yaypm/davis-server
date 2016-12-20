3ec70220b178a92e4a60bd1eab85f5e5640defad
Daniel Dyla <dyladan@gmail.com>
2016-12-20 14:50:17 -0500

    migrations

---
+++ migrations/1482263242723-move problem event.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * Make any changes you need to make to the database here
+ */
+exports.up = function up (done) {
+  done();
+};
+
+/**
+ * Make any changes that UNDO the up function side effects here (if possible)
+ */
+exports.down = function down(done) {
+  done();
+};
