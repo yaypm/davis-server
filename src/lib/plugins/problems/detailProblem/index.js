1b9dc79c88cd65cda2b6fd92326fc74603f64e9f
Daniel Dyla <dyladan@gmail.com>
2017-05-05 10:47:44 -0400

    Add extremely rudimentary listItem() to detailProblem

--- src/lib/plugins/problems/detailProblem/index.js
+++ src/lib/plugins/problems/detailProblem/index.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const Plugin = require("../../../core/plugin");
+const sb = require("../../../util/builder").sb;
+const Dynatrace = require("../../../core/dynatrace");
 
 class DetailProblem extends Plugin {
   constructor() {
@@ -13,7 +15,7 @@ class DetailProblem extends Plugin {
    *
    * @param {IDavisRequest} req
    * @param {Number} choice
-   * @returns
+   * @returns {IDavisResponse}
    *
    * @memberOf DetailProblem
    */
@@ -21,6 +23,15 @@ class DetailProblem extends Plugin {
     const choices = req.context.targets.num.choices;
     return {
       text: `You chose problem ${choices[choice]}`,
+    };
+  }
+
+  async listItem(req, id) {
+    const details = await Dynatrace.problemDetails(req.user, id);
+    return {
+      text: sb(req.user).s("a").h(details.rankedEvents[0].eventType)
+        .s("on").e(details.rankedEvents[0].entityId, details.rankedEvents[0].entityName),
+      value: details,
     };
   }
 }
