e8b1cd626ff62048a8481d858af2dc0e3da8c317
Beemer <Michael.Beemer@dynatrace.com>
2017-05-09 14:38:20 -0400

    Add dynatrace routes

---
+++ src/lib/server/api/dynatrace/aliases.js
@@ -0,0 +1,78 @@
+"use strict";
+
+const aliasRoute = require("express").Router();
+
+const Aliases = require("../../../controllers/aliases");
+const Dynatrace = require("../../../core/dynatrace");
+
+
+aliasRoute.route("/aliases")
+  .get(async (req, res, next) => {
+    try {
+      const {
+        applications,
+        services,
+        hosts,
+        processGroups,
+      } = await Dynatrace.getAllEntities(req.user);
+
+      return res.json({ success: true, applications, services, hosts, processGroups });
+    } catch (err) {
+      return next(err);
+    }
+  })
+  .post(async (req, res, next) => {
+    try {
+      const alias = await Aliases.create({
+        entityId: req.body.entityId,
+        aliases: req.body.aliases,
+        display: {
+          audible: req.body.audible,
+          visual: req.body.visual,
+        },
+      });
+      return res.status(201).json({
+        id: alias._id,
+        message: "Successfully added a new alias.",
+        success: true,
+      });
+    } catch (err) {
+      return next(err);
+    }
+  });
+
+aliasRoute.route("/aliases/:id")
+  .put(async (req, res, next) => {
+    try {
+      await Aliases.update(req.user, req.params.id, req.body);
+      return res.json({ success: true, message: "Successfully updated the alias." });
+    } catch (err) {
+      return next(err);
+    }
+  })
+  .delete(async (req, res, next) => {
+    try {
+      await Aliases.delete(req.user, req.params.id);
+      return res.json({ success: true, message: "Successfully removed the alias." });
+    } catch (err) {
+      return next(err);
+    }
+  });
+
+aliasRoute.route("/aliases/:type(applications|services|hosts|process-groups)")
+  .get(async (req, res, next) => {
+    const type = req.params.type.toLowerCase();
+    try {
+      const aliases = (type === "applications") ? await Dynatrace.getApplications(req.user)
+        : (type === "services") ? await Dynatrace.getServices(req.user)
+        : (type === "hosts") ? await Dynatrace.getHosts(req.user)
+        : (type === "process-groups") ? await Dynatrace.getProcessGroups(req.user)
+        : (function () { throw new Error(`Invalid alias type '${type}'.`); }());
+
+      return res.json({ success: true, [type]: aliases });
+    } catch (err) {
+      return next(err);
+    }
+  });
+
+module.exports = aliasRoute;
--- src/lib/server/api/index.js
+++ src/lib/server/api/index.js
@@ -5,6 +5,7 @@ const Joi = require("joi");
 // davis modules
 const davis = require("../../core/davis");
 const logger = require("../../core/logger");
+const DError = require("../../core/error");
 
 // routes
 const auth = require("./auth");
@@ -15,34 +16,26 @@ const v1 = Router();
 
 v1.use(auth);
 
-v1.post("/ask", async (req, res) => {
-  const schema = Joi.object().keys({ raw: Joi.string().min(1).required() });
-
-  const validate = Joi.validate(req.body, schema);
-
-  if (validate.error) {
-    res.status(400).json({
-      message: validate.error.details[0].message,
-      success: false,
-    });
-    return;
-  }
-
-  const dreq = {
-    raw: validate.value.raw,
-    source: "web",
-    user: req.user,
-  };
-
+v1.post("/ask", async (req, res, next) => {
   try {
+    const schema = Joi.object().keys({ raw: Joi.string().min(1).required() });
+
+    const validate = Joi.validate(req.body, schema);
+
+    if (validate.error) {
+      throw new DError(validate.error.details[0].message, 400);
+    }
+
+    const dreq = {
+      raw: validate.value.raw,
+      source: "web",
+      user: req.user,
+    };
+
     const dres = await davis.ask(dreq);
-    res.status(200).json(dres);
+    res.json(dres);
   } catch (err) {
-    logger.error(err);
-    res.status(500).json({
-      message: "An unhandled error occurred",
-      success: false,
-    });
+    next(err);
   }
 });
 
@@ -50,12 +43,19 @@ v1.use("/system", system);
 v1.use("/dynatrace", dynatrace);
 
 // Global API 404
-v1.use((req, res) => { res.status(404).send({ status: false, message: "Invalid route" }); });
+v1.use((req, res, next) => { next(new DError("Invalid route!", 404)); }); // eslint-disable-line no-unused-vars
 
 // Global API Error Handler
-v1.use((err, req, res) => {
-  logger.error({ err });
-  res.status(500).json({ success: false, message: "An unhandled error occurred" });
+v1.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+  const code = err.statusCode || 500;
+  const message = (err.name === "DavisError") ? err.message : "An unhandled error occurred";
+
+  if (err.name !== "DavisError") {
+    logger.error({ err });
+  } else {
+    logger.debug(message);
+  }
+  res.status(code).json({ success: false, message });
 });
 
 module.exports.v1 = v1;
