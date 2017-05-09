bbb1999b9df64c611445f31e14124f1b11af5501
Beemer <Michael.Beemer@dynatrace.com>
2017-05-09 14:34:56 -0400

    Added aliases

--- src/lib/models/alias.js
+++ src/lib/models/alias.js
@@ -2,23 +2,22 @@
 
 const mongoose = require("mongoose");
 
+const DError = require("../core/error");
+
+require("./tenant");
+
 const aliasSchema = new mongoose.Schema({
+  entityId: { type: String, unique: true, require: [true, "An entity ID is required!"] },
+  tenant: { type: "ObjectId", ref: "Tenant", required: true },
   aliases: [String],
-  createdAt: Date,
   display: {
     audible: String,
     visual: String,
   },
-  entityId: { type: String, unique: true },
-  tenant: String,
-});
+}, { timestamps: true });
 
-aliasSchema.pre("save", function (next) {
-  if (!this.createdAt) {
-    this.createdAt = new Date();
-  }
-  next();
-});
+aliasSchema.index({ entityId: 1, tenant: 1 }, { unique: [true, "This entity already has an alias!"] });
 
+aliasSchema.post("save", DError.handleMongoError);
 module.exports = mongoose.model("Alias", aliasSchema);
 
