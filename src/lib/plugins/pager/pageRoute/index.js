6564afa403b8d70ba1d650044c92d7b16cba30ef
Daniel Dyla <dyladan@gmail.com>
2017-05-09 14:52:52 -0400

    Add failure values for yes/no on pageRoute

--- src/lib/plugins/pager/pageRoute/index.js
+++ src/lib/plugins/pager/pageRoute/index.js
@@ -9,10 +9,16 @@ class PageRoute extends Plugin {
   }
 
   async yes(req, value) {
+    if (!value || !value.target) {
+      return { text: "I'm sorry, but I can't answer that right now." };
+    }
     return this.davis.plugins[value.target]._yes(req, value.id);
   }
 
   async no(req, value) {
+    if (!value || !value.target) {
+      return { text: "I'm sorry, but I can't answer that right now." };
+    }
     return this.davis.plugins[value.target]._no(req, value.id);
   }
 }
