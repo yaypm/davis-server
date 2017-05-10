dc96ec78b2f0b4bcc4abf32c0778984f33bf4eae
Daniel Dyla <dyladan@gmail.com>
2017-05-10 11:16:29 -0400

    Move to the next and previous items in a list directly

--- src/lib/plugins/pager/next/index.js
+++ src/lib/plugins/pager/next/index.js
@@ -10,6 +10,16 @@ class Next extends Plugin {
 
   async ask(req) {
     const paging = req.context.paging;
+    const active = paging.active;
+
+    if (active === -1) {
+      return this.nextPage(req);
+    }
+    return this.nextItem(req);
+  }
+
+  async nextPage(req) {
+    const paging = req.context.paging;
     const page = paging.page;
     const first = page * 3;
     const total = paging.items.length;
@@ -18,6 +28,19 @@ class Next extends Plugin {
     }
     req.context.set("paging.page", paging.page + 1);
     return this.davis.plugins.showPage.run(req);
+  }
+
+  async nextItem(req) {
+    const paging = req.context.paging;
+    const active = paging.active;
+    const next = active + 1;
+    const total = paging.items.length;
+    if (active >= total - 1) {
+      return { text: "You are already on the last item" };
+    }
+    req.context.set("paging.active", next);
+    const item = paging.items[next];
+    return this.davis.plugins[item.target]._yes(req, item.id);
   }
 }
 
--- src/lib/plugins/pager/previous/index.js
+++ src/lib/plugins/pager/previous/index.js
@@ -10,11 +10,34 @@ class Previous extends Plugin {
 
   async ask(req) {
     const paging = req.context.paging;
+    const active = paging.active;
+
+    if (active === -1) {
+      return this.previousPage(req);
+    }
+    return this.previousItem(req);
+  }
+
+  async previousPage(req) {
+    const paging = req.context.paging;
     if (paging.page === 0) {
       return { text: "You are already on the first page" };
     }
     req.context.set("paging.page", paging.page - 1);
     return this.davis.plugins.showPage.run(req);
+  }
+
+  async previousItem(req) {
+    const paging = req.context.paging;
+    const active = paging.active;
+    const next = active - 1;
+
+    if (active === 0) {
+      return { text: "You are already on the first item" };
+    }
+    req.context.set("paging.active", next);
+    const item = paging.items[next];
+    return this.davis.plugins[item.target]._yes(req, item.id);
   }
 }
 
