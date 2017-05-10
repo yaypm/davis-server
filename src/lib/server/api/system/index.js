4b0ee29e30be6124f583d5ccfd253801c4170110
Beemer <Michael.Beemer@dynatrace.com>
2017-05-09 23:05:04 -0400

    Added version and user route

--- src/lib/server/api/system/index.js
+++ src/lib/server/api/system/index.js
@@ -1,14 +1,40 @@
 const Router = require("express").Router;
 const Users = require("../../../controllers/users");
 
+const version = require("../../../../../package.json").version;
+
 const system = Router();
 
+system.get("/version", (req, res) => {
+  res.json({
+    success: true,
+    version,
+  });
+});
+
+system.get("/user", (req, res) => {
+  res.json({
+    success: true,
+    email: req.user.email,
+    name: {
+      first: req.user.firstName,
+      last: req.user.lastName,
+    },
+    timezone: req.user.timezone,
+  });
+});
+
+// TODO secure route
 system.get("/users", async (req, res) => {
   const users = await Users.getAll();
 
   res.json(users.map(user => ({
-    first: user.firstName,
-    last: user.lastName,
+    email: user.email,
+    name: {
+      first: user.firstName,
+      last: user.lastName,
+    },
+    timezone: user.timezone,
   })));
 });
 
